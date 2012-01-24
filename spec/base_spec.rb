4c4ce6d490da3e7ef5e41ef6558cbf0fbdcfe012
Dario Seminara <darios3@gmail.com>
2012-01-24 19:42:01 -0300

    refactor on method handler specs

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -3,7 +3,7 @@ require "define_method_handler"
 
 describe "define_method_handler" do
   
-  it "single method handler without condition should act as the a method" do
+  it "single method handler without condition should act as a method" do
     class CHAIN1
       define_method_handler(:foo) do
         100
@@ -14,14 +14,14 @@ describe "define_method_handler" do
   end
 
   
-  it "single method handler without condition should act as the a method and this method should accept blocks" do
-    class CHAIN1
+  it "single method handler without condition should act as a method and this method should accept blocks" do
+    class CHAIN2
       define_method_handler(:foo) do |&blk|
         blk.call + 1
       end
     end
     
-    CHAIN1.new.foo{99}.should be == 100
+    CHAIN2.new.foo{99}.should be == 100
   end
   
 end
