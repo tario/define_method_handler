e06edef34559d31a5406bc2727695abab35af36f
Dario Seminara <darios3@gmail.com>
2012-01-24 20:15:48 -0300

    +1 failing test: two methods with same name and with no conditions should execute the implementation with higher priority

--- spec/priority_spec.rb
+++ spec/priority_spec.rb
@@ -16,5 +16,18 @@ describe "define_method_handler" do
     
     CHAIN1_1.new.foo.should be == 2
   end
-  
+
+   it "two methods with same name and with no conditions should execute the implementation with higher priority" do
+    class CHAIN1_2
+      define_method_handler(:foo, :priority => 100) {|x|
+        1
+      }
+
+      define_method_handler(:foo, :priority => 1) {|x|
+        2
+      }
+    end
+    
+    CHAIN1_2.new.foo.should be == 1
+  end
 end
