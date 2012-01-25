cf4305af11b5eb4f41c7f327340aeea0ce1808fb
Dario Seminara <darios3@gmail.com>
2012-01-24 21:00:06 -0300

    +1 passing test: one method handler defined with group only should run after a group disable block

--- spec/group_spec.rb
+++ spec/group_spec.rb
@@ -25,5 +25,19 @@ describe "define_method_handler" do
       chain.foo.should be == nil
     end
   end
+ 
+  it "one method handler defined with group only should run after a group disable block" do
+    class CHAIN2_1
+      define_method_handler(:foo, :group => :testgroup) {
+        10
+      }
+    end
+    
+    chain = CHAIN2_1.new
+    chain.disable_handler_group(:testgroup) do
+    end
+    
+    chain.foo.should be == 10
+  end 
 
 end
