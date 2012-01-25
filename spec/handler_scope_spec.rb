d324e88a41905f010f3a9a60b53becf2a1640b80
Dario Seminara <darios3@gmail.com>
2012-01-24 22:32:43 -0300

    +1 failing test: method handler defined after handler_scope block should not be affected

--- spec/handler_scope_spec.rb
+++ spec/handler_scope_spec.rb
@@ -17,4 +17,24 @@ describe "define_method_handler" do
       chain.foo.should be == nil
     end
   end
+  
+  
+  it "method handler defined after handler_scope block should not be affected" do
+    class CHAIN5_1
+      handler_scope(:group => :testgroup) do
+        define_method_handler(:foo) do
+          100
+        end
+      end
+
+      define_method_handler(:bar) do
+        100
+      end
+    end
+
+    chain = CHAIN5_1.new
+    chain.disable_handler_group(:testgroup) do
+      chain.bar.should be == 100
+    end
+  end
 end
