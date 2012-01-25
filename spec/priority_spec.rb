ecdc61cd64907a1c4ffc98fc044ff5669b3c95d4
Dario Seminara <darios3@gmail.com>
2012-01-24 22:13:25 -0300

    fixed specs (arguments on define_method_handler)

--- spec/priority_spec.rb
+++ spec/priority_spec.rb
@@ -5,11 +5,11 @@ describe "define_method_handler" do
  
   it "two methods with same name and with no conditions should execute the last implementation" do
     class CHAIN1_1
-      define_method_handler(:foo) {|x|
+      define_method_handler(:foo) {
         1
       }
 
-      define_method_handler(:foo) {|x|
+      define_method_handler(:foo) {
         2
       }
     end
@@ -19,11 +19,11 @@ describe "define_method_handler" do
 
    it "two methods with same name and with no conditions should execute the implementation with higher priority" do
     class CHAIN1_2
-      define_method_handler(:foo, :priority => 100) {|x|
+      define_method_handler(:foo, :priority => 100) {
         1
       }
 
-      define_method_handler(:foo, :priority => 1) {|x|
+      define_method_handler(:foo, :priority => 1) {
         2
       }
     end
