a09a624354812c24b34971a3cb407429c861cede
Dario Seminara <darios3@gmail.com>
2012-01-24 22:19:31 -0300

    +1 passing test: should allow calling instance methods from method handler

--- spec/scope_spec.rb
+++ spec/scope_spec.rb
@@ -12,4 +12,18 @@ describe "define_method_handler" do
     chain = CHAIN3_1.new
     chain.foo.should be == chain
   end
+  
+  it "should allow calling instance methods from method handler" do
+    class CHAIN3_2
+      def helper
+        92
+      end
+      
+      define_method_handler(:foo) do
+        helper
+      end
+    end
+    chain = CHAIN3_2.new
+    chain.foo.should be == 92
+  end
 end
