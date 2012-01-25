9abc6fd6140d96ab5298d7c72ebeeb79f6b65864
Dario Seminara <darios3@gmail.com>
2012-01-24 22:38:11 -0300

    +1 failing test: nested handler_scopes should merge options

--- spec/handler_scope_spec.rb
+++ spec/handler_scope_spec.rb
@@ -20,7 +20,7 @@ describe "define_method_handler" do
   
   
   it "method handler defined after handler_scope block should not be affected" do
-    class CHAIN5_1
+    class CHAIN4_2
       handler_scope(:group => :testgroup) do
         define_method_handler(:foo) do
           100
@@ -32,9 +32,32 @@ describe "define_method_handler" do
       end
     end
 
-    chain = CHAIN5_1.new
+    chain = CHAIN4_2.new
     chain.disable_handler_group(:testgroup) do
       chain.bar.should be == 100
+    end
+  end
+  
+  it "nested handler_scopes should merge options" do
+    class CHAIN4_3
+      handler_scope(:group => :testgroup) do
+        handler_scope(:priority => 100) do
+          define_method_handler(:foo) do
+            100
+          end
+        end
+      end
+
+      define_method_handler(:foo) do
+        200
+      end
+    end
+
+    chain = CHAIN4_3.new
+    chain.foo.should be == 100
+
+    chain.disable_handler_group(:testgroup) do
+      chain.foo.should be == 200
     end
   end
 end
